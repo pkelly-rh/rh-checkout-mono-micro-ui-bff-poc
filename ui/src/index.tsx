1310395bcabd054892597d663780a0435b884378
Patrick <pkelly@rh.com>
2023-10-24 08:18:42 -0700

    apollo client for ui integrated roughly

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import "./main.css";
 import "./images/icons/favicon.ico";
 import { RouterProvider } from "react-router-dom";
-
 import { router } from "./router";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+
 
 if (module && module.hot) {
   module.hot.accept();
 }
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- ui/src/pages/ShoppingCart/ShoppingCart.tsx
+++ ui/src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery, gql } from '@apollo/client';
+
+
+const queryCart = gql`
+  query Cart {
+    cart (id: "111") {
+      id
+    }
+  }
+`
 
 export const ShoppingCart = () => {
+  const {data} = useQuery(queryCart, {variables: {id: "0000"}})
+
+
+
   return <div>
     <h1>
       ShoppingCart
       </h1>
-    
-    
+      <h3>cartID {data?.cart?.id}</h3>
+
       <Link to="/address">
         <button>checkout</button>
       </Link>
